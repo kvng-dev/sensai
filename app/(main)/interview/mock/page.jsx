2757ee0cd4a5a0d01faa0a0174a4e30bb289b927
kvng-dev <oghuanlan@gmail.com>
2025-02-02 17:40:17 +0100

    add interview page and implementation

--- app/(main)/interview/mock/page.jsx
+++ app/(main)/interview/mock/page.jsx
@@ -1,4 +1,36 @@
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import Quiz from "../_components/quiz";
+
 const MockInterview = () => {
-  return <div>MockInterview</div>;
+  return (
+    <div className="container mx-auto space-y-4 py-6">
+      <div className="flex flex-col space-y-2 mx-2">
+        <Link href={"/interview"}>
+          <Button variant="link" className="gap-2 pl-0">
+            <ArrowLeft className="h-4 w-4" />
+            Back to Interview Prep
+          </Button>
+        </Link>
+
+        <div>
+          <h1 className="text-6xl font-bold gradient-title">Mock Interview</h1>
+          <p className="text-muted-foreground">
+            This is a mock interview preparation platform, designed to help
+            candidates prepare for various job interviews. You can practice
+            questions, practice coding, and practice coding-related skills. Feel
+            free to explore the platform and create your own customized practice
+            sets.{" "}
+            <strong>
+              Remember, this is a mock interview, and you should not expect to
+              pass.
+            </strong>
+          </p>
+        </div>
+      </div>
+      <Quiz />
+    </div>
+  );
 };
 export default MockInterview;
--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -1,4 +1,20 @@
-const Interview = () => {
-  return <div>Interview</div>;
+import { getAssessments } from "@/actions/interview";
+import StatsCards from "./_components/stats-card";
+import PerformanceChart from "./_components/performance-chart";
+import QuizList from "./_components/quiz-list";
+
+const Interview = async () => {
+  const assessments = await getAssessments();
+  return (
+    <div>
+      <h1 className="text-6xl font-bold gradient-title">Interview Prep</h1>
+
+      <div className="space-y-6">
+        <StatsCards assessments={assessments} />
+        <PerformanceChart assessments={assessments} />
+        <QuizList assessments={assessments} />
+      </div>
+    </div>
+  );
 };
 export default Interview;
